90a7f3f47f32959c9c637875de2f6021fa53de0e
souanzy <sou.anzy@gmail.com>
2023-04-27 01:11:50 +0900

    a

---
+++ mathjax_macro.js
@@ -0,0 +1,18 @@
+/* MathJax.Hub.Config({
+    TeX:{
+        Macros:{
+            Div: '{\\mathrm{div}}'
+        }
+    }
+    });
+MathJax.Ajax.loadComplete("https://whyitsso.net/mathjax_macro.js");
+*/
+
+window.MathJax = {
+    tex: {
+        tags: "ams",
+        macros: {
+            
+        }
+    }
+}
